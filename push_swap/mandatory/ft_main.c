28e2d935ff3441a9c131af4fee8d9c8ee424197d
r4pidstart <r4pidstart@gmail.com>
2022-09-26 19:56:27 +0900

    modified: if stack is already sorted, does nothing

--- push_swap/mandatory/ft_main.c
+++ push_swap/mandatory/ft_main.c
@@ -6,7 +6,7 @@
 /*   By: tjo <tjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 02:43:26 by tjo               #+#    #+#             */
-/*   Updated: 2022/09/25 15:47:47 by tjo              ###   ########.fr       */
+/*   Updated: 2022/09/26 19:55:24 by tjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,41 @@ static int	print_error(void)
 	return (!write(2, "Error\n", 6));
 }
 
+static int	check(void)
+{
+	int		flag;
+	long	prev;
+	int		tmp;
+
+	flag = 0;
+	prev = (long)INT32_MIN - 1;
+	flag += dlist(1, size, 0);
+	while (dlist(0, size, 0))
+	{
+		tmp = dlist(0, pop_front, 0);
+		flag += (prev > tmp);
+		prev = tmp;
+	}
+	if (!flag)
+	{
+		dlist(0, delete, 0);
+		dlist(1, delete, 0);
+	}
+	return (flag);
+}
+
 int	main(int argc, char **argv)
 {
 	int	idx;
 
-	if (argc == 1)
-		return (0);
 	if (dlist(0, init, 0) || dlist(1, init, 0))
 		return (print_error());
 	idx = 1;
 	while (idx < argc)
 		if (read_argument(argv[idx++]))
 			return (print_error());
+	if (argc == 1 || !check())
+		return (0);
 	if (dlist(0, size, 0) < 7)
 	{
 		if (is_duplicated() || compress() || sort_small(0))
